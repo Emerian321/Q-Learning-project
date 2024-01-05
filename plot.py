f86dcfce911f4efbc1c8108e33a59f9b77507fc9
emerian320 <emile.merian@ulb.be>
2024-01-05 18:22:39 +0100

    update plot

--- plot.py
+++ plot.py
@@ -1,13 +1,25 @@
 import matplotlib.pyplot as plt
+import numpy as np
+
 
 def plot_graph(filename, graph_title, ylabel, dataset):
+    x = np.linspace(1, len(dataset[0])+1, len(dataset[0]))
+    y = dataset[0]
+    # fit a linear curve and estimate its y-values and their error.
+    a, b = np.polyfit(x, y, deg=1)
+    y_est = a * x + b
+    y_err = x.std() * np.sqrt(1/len(x) +
+                            (x - x.mean())**2 / np.sum((x - x.mean())**2))
 
-    plt.scatter(range(1, len(dataset[0])+1), dataset[0], label='Independant Learners', alpha=0.1)
-    plt.scatter(range(1, len(dataset[1])+1), dataset[1], label='Joint-Action Learner', alpha=0.1)
-    plt.scatter(range(1, len(dataset[2])+1), dataset[2], label='Sparse Cooperative algorithm', alpha=0.1)
+    fig, ax = plt.subplots()
+    ax.plot(x, y_est, '-')
+    ax.fill_between(x, y_est - y_err, y_est + y_err, alpha=0.2)
+    ax.plot(x, y, 'o', color='tab:brown')
+
     
     plt.xlabel('Episodes')
     plt.ylabel(ylabel)
-    plt.title(graph_title + "in environment" + filename)
+    plt.title(graph_title + " in environment " + filename)
     plt.legend()
     plt.show()
+
